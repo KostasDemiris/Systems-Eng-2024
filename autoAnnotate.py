5786f43277818c535dc58f216aa6c5374b295036
KostasDemiris <kostas.demiris@icloud.com>
2024-11-26 09:45:41 +0000

    Automatic Annotation using MapBoxTiles
    This is a reasonably functional prototype, might need some training for coverage/ obstruction detection, but performs reasonably well in end-to-end functionality.

--- autoAnnotate.py
+++ autoAnnotate.py
@@ -1,18 +1,23 @@
-# Requires overpy, the library that lets us access the Overpass API through python
-import random
-MAPBOX_API_KEY = "pk.eyJ1Ijoia29uc3RhbnRpbm9zLWRlbWlyaXMiLCJhIjoiY20zdmVuOGtiMDlzdjJsc2dsaHg3d2w3dSJ9.Kc4Z9eZ7_nycHeacJueb0A"
-
+# Requires overpy, the library that lets us access the Overpass API through python, DON'T DELETE
 import overpy
 from geopy.geocoders import Nominatim
 import requests
 
+MAPBOX_API_KEY = ("pk.eyJ1Ijoia29uc3RhbnRpbm9zLWRlbWlyaXMiLCJhIjoiY20zdmVuOGtiMDlzdjJsc2dsaHg3d2w3dSJ9.Kc4Z9eZ7_"
+                  "nycHeacJueb0A")  # This is kostas's personal API key, might have to replace with a business one...
+crosswalk_images_folder = "crosswalk_images"
 
-def get_cross_walk_location(location_geo_code):
+
+def get_cross_walk_location(location_geo_code, bounds=0.001):
     over_api = overpy.Overpass()
     geo_locator = Nominatim(user_agent="crosswalk_locator")
     location = geo_locator.geocode(location_geo_code)
-    geo_bounding_box = (location.latitude - 0.01, location.longitude - 0.01,
-                        location.latitude + 0.01, location.longitude + 0.01)  # South, West, North, East
+
+    if not location:
+        raise ValueError("This location could not be geocoded.")
+
+    geo_bounding_box = (location.latitude - bounds, location.longitude - bounds,
+                        location.latitude + bounds, location.longitude + bounds)  # South, West, North, East
 
     query = f"""
     [out:json][timeout:25];
@@ -20,16 +25,16 @@ def get_cross_walk_location(location_geo_code):
       node["highway"="crossing"]({geo_bounding_box[0]},{geo_bounding_box[1]},{geo_bounding_box[2]},{geo_bounding_box[3]});
     );
     out body;
-    """  # Should return all crosswalks within 0.01 longitude/ latitude points,
+    """  # Should return all crosswalks within {bounds} longitude/ latitude points,
 
     result = over_api.query(query)
 
     crosswalks = []
     for node in result.nodes:
         crosswalks.append((node.lat, node.lon))
-        # print(f"Crosswalk at latitude: {node.lat}, longitude: {node.lon}")
     # Now we have a list of all the (latitude, longitude) pairs in that area,
     # and can use it access the images of those areas for annotation
+
     return crosswalks
 
 
@@ -52,14 +57,15 @@ def get_mapbox_aerial_image(location, output_file_name, zoom=18, size="600x600",
         print("Error, Error, Error!", response.status_code, response.text)
 
 
-def get_crosswalk_images(geo_code):
+def get_crosswalk_images(geo_code, folder=crosswalk_images_folder, file_extension="png"):
     crosswalk_image_files = []
-    crosswalk_set = get_cross_walk_location(geo_code)
+    crosswalk_set = get_cross_walk_location(geo_code, bounds=0.1)
     for crosswalk in crosswalk_set:
         formatted_crosswalk = list(map(float, crosswalk))
         stored_file_name = str(formatted_crosswalk)
         try:
-            get_mapbox_aerial_image(formatted_crosswalk, f"{formatted_crosswalk}.png")
+            get_mapbox_aerial_image(formatted_crosswalk,
+                                    f"{folder}/{formatted_crosswalk}.{file_extension}")
             crosswalk_image_files.append(stored_file_name)
         except Exception as e:
             print(e)
